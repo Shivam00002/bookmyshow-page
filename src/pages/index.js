39578312835bfcd6893a2ab2e5f914509d1b8f78
Shivam Dubey <108024596+Shivam00002@users.noreply.github.com>
2023-05-16 12:33:01 +0530

    added

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import Events from './Events'
+import Navbar from '@/components/Navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,6 +16,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
     <Events/>
+    {/* <Navbar/> */}
     </>
   )
 }
