03db968a75157583210f063a3860c31b04f6cb75
Shivam Dubey <108024596+Shivam00002@users.noreply.github.com>
2023-08-11 23:29:03 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,15 +8,18 @@ import { useState } from "react";
 const Navbar =()=>{
     const [ isOpen , setIsOpen] = useState(false)
 
+
     const toggleMenu =() =>{setIsOpen(!isOpen)}
 
     return (
         <>
         <div className="navbar" id="nav-menu">
             <div id="nav-logo"  className={!isOpen ? "my-logo-open" : "my-logo-close"}>
+
                 <Image src="https://res.cloudinary.com/admitad-gmbh/image/upload/h_0.5,f_auto/ixaa8tyj24yv45cmgysl.png" height="33" width="95" alt="nav-logo"/>
             </div>
             <div className="menu-button" onClick={toggleMenu}><MenuOutlined  className="menu"/></div>
+
             <nav className={!isOpen ? "nav-close" : "nav-open"}>
                 <Link className="link" href="Workshops">Workshops</Link>
                 <Link className="link" href="Meetups">Meetups</Link>
