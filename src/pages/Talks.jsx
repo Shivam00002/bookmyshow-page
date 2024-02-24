c2b39726bf7a01e3a81d520c508931bc1b2367dd
Shivam Dubey <108024596+Shivam00002@users.noreply.github.com>
2024-02-24 18:23:26 +0530

    Update Talks.jsx

--- src/pages/Talks.jsx
+++ src/pages/Talks.jsx
@@ -4,6 +4,7 @@ import styles from '../styles/All.module.css'
 const Talks=()=>{
     return (
         <>
+
         <div className={styles.all}>
             <h1>Welcome to Talks</h1>
             <Link href="/">Go Back</Link>
