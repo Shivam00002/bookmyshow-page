553c78cc825cb30f5f70a3c09f8cbd4cd0f143ac
Shivam Dubey <108024596+Shivam00002@users.noreply.github.com>
2024-05-24 23:53:23 +0530

    Update Meetups.jsx

--- src/pages/Meetups.jsx
+++ src/pages/Meetups.jsx
@@ -4,8 +4,6 @@ import styles from '../styles/All.module.css'
 const Meetups=()=>{
     return (
         <>
-
-
         <div className={styles.all}>
             <h1>Welcome to Meetups</h1>
             <Link href="/">Go Back</Link>
